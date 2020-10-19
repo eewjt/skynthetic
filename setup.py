182416685dc4f2ed0391c039f0c5def5f7e92f83
Euan Soutter <euansoutter@MacBook-Pro.local>
2020-10-19 12:23:16 +0100

    setup

--- setup.py
+++ setup.py
@@ -4,25 +4,6 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="skynthetic", # Replace with your own username
-    version="1.0",
-    author="Euan Soutter",
-    author_email="euan.soutter@manchester.ac.uk",
-    description="sketch to seismic",
-    long_description="turn images into synthetic seismograms",
-    long_description_content_type="text/markdown",
-    url="https://github.com/eslrgs/skynthetic.git",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
-from setuptools import setup
-
-setup(
    name="skynthetic", # Replace with your own username
     version="1.0",
     author="Euan Soutter",
@@ -31,7 +12,7 @@ setup(
     long_description="turn images into synthetic seismograms",
     long_description_content_type="text/markdown",
     url="https://github.com/eslrgs/skynthetic.git",
-   packages=['skynthetic'],  #same as name
-   install_requires=['matplotlib==3.2.2', 'bruges==0.3.4', 
-   'numpy==1.18.5', 'Pillow==8.0.0', 'IPython', 'jupyter==1.0.0'] #external packages as dependencies
+    packages=['skynthetic'],  #same as name
+    install_requires=['matplotlib==3.2.2', 'bruges==0.3.4', 
+    'numpy==1.18.5', 'Pillow==8.0.0', 'IPython', 'jupyter==1.0.0'] #external packages as dependencies
 )
